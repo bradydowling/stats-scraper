c65f603e97911c970c5974642290bb982dae0329
bradydowling <bradydowling@gmail.com>
2020-01-10 23:43:03 -0500

    Add random tools

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const rp = require('request-promise');
 const cheerio = require('cheerio');
+
+const baseUrl = `https://www.churchofjesuschrist.org/study/scriptures/nt/${book}/${chapter}?lang=eng`;
+const options = {
+  uri: baseUrl,
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
+
+// Get one scripture and count words
+rp(options)
+  .then(function($){
+    let scriptureText = '';
+    for (let i = versesStart; i <= versesEnd; i++) {
+      scriptureText += $(`#p${i}.verse`).text().split(' ').slice(1).join(' ') + ' ';
+    }
+    const wordCount = scriptureText.split(' ').length;
+    const characterCount = scriptureText.split('').length;
+    console.log(`${scripture}: ${wordCount} words, ${characterCount} characters`);
+  })
+  .catch(function(err){
+    //handle error
+  });
