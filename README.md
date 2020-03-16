7be5678d3b0afaf67b1ce8245c3df98308c0538d
bradydowling <bradydowling@gmail.com>
2020-03-16 09:20:03 -0400

    Update stuff to use different stat sources

--- README.md
+++ README.md
@@ -14,3 +14,22 @@ To get an array of the top 50 all-time NBA scorers:
 ```
 
 **Note:** This could eventually be turned into a Cheerio script.
+
+# Player Career Stats
+
+**Note:** Until this is turned into a Cheerio script, you'll need to do this for all 50 players on the leading scorers list.
+
+1. Open up the stats page pertain to your player of choice at `https://www.basketball-reference.com/players/${lastName.toLowerCase().slice(0, 1)}/${lastName.toLowerCase().slice(0, 5)}${firstName.toLowerCase().slice(0, 2)}01.html`
+1. Open your browser console
+
+To get an array stat categories from the table header:
+```
+const statHeaders = [...document.querySelectorAll('#totals thead th')].map(item => item.dataset.stat)
+```
+
+To get an array of a player's stats by seasons:
+
+TODO:
+```
+[...document.querySelectorAll('#totals tbody tr')].map(item => item.querySelector('td:nth-of-type(2) a').innerText)
+```
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const leadingScorers = require('./leadingScorers');
-const rp = require('request-promise');
+const fetch = require('node-fetch');
 const $ = require('cheerio');
 
 // TODO: Change to use https://www.landofbasketball.com/nba_players_stats/b/${firstName}_${lastName}_tot.htm
@@ -9,32 +9,64 @@ const $ = require('cheerio');
 // Convert array to CSV
 
 async function getCareerStats({ firstName, lastName }) {
-  const baseUrl = `https://herosports.com/nba/player/${firstName.toLowerCase()}-${lastName.toLowerCase()}-stats`;
+  const baseUrl = `https://www.basketball-reference.com/players/${lastName.toLowerCase().slice(0, 1)}/${lastName.toLowerCase().slice(0, 5)}${firstName.toLowerCase().slice(0, 2)}01.html`;
 
-  rp(baseUrl)
-  .then(function(html){
-    const statHeaders = [];
-
-    $('#table3 thead tr acronym', html).each(function(i, elem) {
-      statHeaders[i] = $(this).attr('title').trim();
-    });
-
-    const yearlyStats = [];
-    $('#table3 tbody tr', html).each(function(i, elem) {
-      const thisYear = {};
-      const tableData = $(this).find('td');
-      tableData.each(function(index, item) {
-        const key = statHeaders[index];
-        thisYear[key] = $(this).text();
-      });
-      thisYear.index = i;
-      yearlyStats.push(thisYear);
-    });
-    return yearlyStats;
+  fetch(baseUrl, {
+    method: 'GET',
+    headers: {
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.5",
+      "Cache-Control": "max-age=0",
+      "Connection": "keep-alive",
+      "Cookie": "auth_checked=true; srcssfull=yes; is_live=true; SR_user=",
+      "DNT": "1",
+      "Host": "www.basketball-reference.com",
+      "Upgrade-Insecure-Requests": "1",
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:74.0) Gecko/20100101 Firefox/74.0"
+    }
   })
+  .then(response => response.text())
+  .then(parseYearlyStats)
   .catch(function(err){
     //handle error
   });
+}
+
+function parseYearlyStats(html) {
+  const statHeaders = getStatHeaders(html);
+  const yearlyStats = getYearlyStats(html, statHeaders);
+  return yearlyStats;
+}
+
+function getStatHeaders(html) {
+  const statHeaders = [];
+
+  console.log($('thead', html).length);
+  $('#totals thead tr th', html).each(function(i, elem) {
+    console.log(i);
+    console.log(elem);
+    console.log($(this).text());
+    statHeaders[i] = $(this).attr('data-stat');
+  });
+  console.log('statHeaders');
+  console.log(statHeaders);
+  return statHeaders;
+}
+
+function getYearlyStats() {
+  const yearlyStats = [];
+  $('#totals tbody tr', html).each(function(i) {
+    const thisYear = {};
+    const tableData = $(this).find('td');
+    tableData.each(function(index, item) {
+      const key = statHeaders[index];
+      thisYear[key] = $(this).text();
+    });
+    thisYear.index = i;
+    yearlyStats.push(thisYear);
+  });
+  return yearlyStats;
 }
 
 async function getPlayersStats(players) {
@@ -50,4 +82,6 @@ const allPlayers = leadingScorers.map(player => {
   return { firstName, lastName };
 });
 
-getPlayersStats(allPlayers);
+const testPlayer = [{ firstName: 'Dirk', lastName: 'Nowitzki'}];
+
+getPlayersStats(testPlayer);
