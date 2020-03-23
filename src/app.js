ce559c0756d6313feac05457cb8e815ab0758e98
bradydowling <bradydowling@gmail.com>
2020-03-22 22:14:27 -0400

    Slowly converting from observable

--- src/app.js
+++ src/app.js
@@ -6,39 +6,42 @@ const height = 500;
 const adj = 20;
 
 // we are appending SVG first
-const svg = d3.select('div#container').append('svg')
-    .attr('preserveAspectRatio', 'xMinYMin meet')
-    .attr('viewBox', `-${adj} -${adj} ${width + adj} ${height + adj}`)
-    .style('padding', 5)
-    .style('margin', 5)
-    .classed('svg-content', true);
+const svg = d3
+  .select('div#container')
+  .append('svg')
+  .attr('preserveAspectRatio', 'xMinYMin meet')
+  .attr('viewBox', `-${adj} -${adj} ${width + adj} ${height + adj}`)
+  .style('padding', 5)
+  .style('margin', 5)
+  .classed('svg-content', true);
 
 //-----------------------DATA PREPARATION------------------------//
-const dataset = d3.csv('data.csv');
-dataset.then((data) => {
-  data.map((d) => {
+const dataset = d3.csv('./data/cats.csv');
+dataset.then(data => {
+  data.map(d => {
     d.val = +d.val;
     return d;
   });
 });
 
 //---------------------------BAR CHART---------------------------//
-dataset.then((data) => {
-  svg.selectAll('div')
-  .data(data)
-  .enter()
-  .append('rect')
-  .attr('class', 'bar')
-  .attr('x', (d, i) => {
-    for (i>0; i < data.length; i++) {
-      return i * 21;
-    }
-  })
-  .attr('y', (d) => {
-      return height - (d.val*10);
-  })
-  .attr('width', 20)
-  .attr('height', (d) => {
-    return d.val*10;
-  });
+dataset.then(data => {
+  svg
+    .selectAll('div')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d, i) => {
+      for (i > 0; i < data.length; i++) {
+        return i * 21;
+      }
+    })
+    .attr('y', d => {
+      return height - d.val * 10;
+    })
+    .attr('width', 20)
+    .attr('height', d => {
+      return d.val * 10;
+    });
 });
